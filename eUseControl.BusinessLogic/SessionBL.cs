b73cd8bc0fb44dc0fd83dbf1e028b9878005c409
MaximSaveliev <maxim.saveliev@mail.ru>
2023-04-30 02:50:11 +0300

    Added Buissnes Logic

---
+++ eUseControl.BusinessLogic/SessionBL.cs
@@ -0,0 +1,20 @@
+﻿using eUseControl.BusinessLogic.Core;
+using eUseControl.BusinessLogic.Interfaces;
+using eUseControl.Domain.Entities.ResActionStatus;
+using eUseControl.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUseControl.BusinessLogic
+{
+    public class SessionBL : UserApi, ISession
+    {
+        //public ActionStatus UserLogin(ULoginData data)
+        //{
+        //    return UserLogData(data);
+        //}
+    }
+}
---
+++ eUseControl.Domain/Entities/ResActionStatus/ActionStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUseControl.Domain.Entities.ResActionStatus
+{
+    public class ActionStatus
+    {
+        public bool Status { get; set; }
+        public bool StatusMsg { get; set; }
+    }
+}
